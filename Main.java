ca5a72d4dbeb0299a3c32aa1f9d82ce0e3f9d664
pelikenmag <kspako@keradom.org>
2016-05-27 16:43:24 +0300

    Added and tested getDameNumber()

--- Main.java
+++ Main.java
@@ -7,7 +7,12 @@ public class Main{
 
 		board.printBoard();
 		playOut(board, Board.FRIENDLY);
+		
+		board.setPoint(new Point(2,1),Board.EMPTY);
+		board.setPoint(new Point(1,2),Board.EMPTY);
 		board.printBoard();
+		System.out.printf("Dame number: %d",getDameNumber(new Point(1,1), board));
+
 	}
 	
 	static void playOut(Board board, int first_stone){
@@ -15,7 +20,8 @@ public class Main{
 		Random random = new Random();
 		int random_point;
 		int stone = first_stone;
-		
+
+
 		free_points = getFreePoints(board);
 		while (!free_points.isEmpty()){
 			random_point = random.nextInt(free_points.size());
@@ -35,11 +41,31 @@ public class Main{
 	static ArrayList<Point> getFreePoints(Board board){
 		int i,j;
 		ArrayList<Point> points = new ArrayList<Point>();
+		Point p;
+
 		for (i = 0; i < board.getSize(); i++)
-			for (j = 0; j < board.getSize(); j++)
-				if (board.getPoint(new Point(i,j)) == Board.EMPTY)
-					points.add(new Point(i,j));
+			for (j = 0; j < board.getSize(); j++){
+				p = new Point(i,j);
+				if (board.getPoint(p) == Board.EMPTY){
+					points.add(p);
+				}
+			}
 		return points;
 	}
+	static int getDameNumber(Point p, Board board){
+		int dame_count = 0;
+		if (board.getPoint(new Point(p.x+1, p.y)) == Board.EMPTY)
+			dame_count++;
+		if (board.getPoint(new Point(p.x-1, p.y)) == Board.EMPTY)
+			dame_count++;
+		if (board.getPoint(new Point(p.x, p.y+1)) == Board.EMPTY)
+			dame_count++;
+		if (board.getPoint(new Point(p.x, p.y-1)) == Board.EMPTY)
+			dame_count++;
+
+		return dame_count;
+
+	}
+
 }
 
