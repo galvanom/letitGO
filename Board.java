9305f2c7d05d6c35627037f6e8e044ab7980d30b
pelikenmag <kspako@keradom.org>
2016-06-01 16:42:48 +0300

    New functions
    New functions for playout. Copy constructor to Board. Changed given
    parameters to Board.getStone() and Board.setStone().

--- Board.java
+++ Board.java
@@ -1,6 +1,6 @@
 
 public class Board{
-	int[][] board;
+	int[][] board, ko_board;
 	int size;
 	public static final int EMPTY = 0;
 	public static final int BORDER = 1;
@@ -23,6 +23,23 @@ public class Board{
 			board[i][this.size-1] = BORDER;
 
 		}
+	}
+	Board(Board otherBoard){
+		int i,j;
+		this.size = otherBoard.getSize()+2;
+		this.board = new int[this.size][this.size];
+		
+		for (i = 0; i < this.size; i++){
+			board[0][i] = BORDER;
+			board[i][0] = BORDER;
+			board[this.size-1][i] = BORDER;
+			board[i][this.size-1] = BORDER;
+
+		}
+		for (i = 0; i < this.size-2; i++)
+			for (j = 0; j < this.size-2; j++)
+				this.board[i+1][j+1] = otherBoard.getPoint(i,j);
+
 	}
 	void printBoard(){
 		int i, j;
@@ -47,12 +64,12 @@ public class Board{
 		System.out.println();
 		}
 	}
-	boolean setPoint(Point p, int stone){
-		board[p.x + 1][p.y + 1] = stone;
+	boolean setPoint(int x, int y, int stone){
+		board[x + 1][y + 1] = stone;
 		return true;
 	}
-	int getPoint(Point p){
-		return board[p.x + 1][p.y + 1];
+	int getPoint(int x, int y){
+		return board[x + 1][y + 1];
 	}
 
 	int getSize(){
--- Main.java
+++ Main.java
@@ -4,14 +4,27 @@ public class Main{
 	public static void main(String[] args){
 	
 		Board board = new Board(9);
+		Point p;
 
 		board.printBoard();
 		playOut(board, Board.FRIENDLY);
-		
-		board.setPoint(new Point(2,1),Board.EMPTY);
-		board.setPoint(new Point(1,2),Board.EMPTY);
-		board.printBoard();
-		System.out.printf("Dame number: %d",getDameNumber(new Point(1,1), board));
+
+		//<isFriendlySingleEyePoint() test>
+		/*
+		board.setPoint(0,1, Board.FRIENDLY);
+		board.setPoint(2,1, Board.FRIENDLY);
+		board.setPoint(1,0, Board.FRIENDLY);
+		board.setPoint(1,2, Board.FRIENDLY);
+		p = new Point(1,1);
+		if (getDameNumber(p, board) == 0){
+			boolean is_friendly = isFriendlySingleEyePoint(p, board);
+			if (is_friendly)
+				System.out.printf("The single eye point is FRIENDLY\n");
+			else
+				System.out.printf("The single eye point is ENEMY\n");
+		}
+		*/
+		//</isFriendlySingleEyePoint() test>
 
 	}
 	
@@ -20,12 +33,13 @@ public class Main{
 		Random random = new Random();
 		int random_point;
 		int stone = first_stone;
-
+		Point p;
 
 		free_points = getFreePoints(board);
 		while (!free_points.isEmpty()){
 			random_point = random.nextInt(free_points.size());
-			board.setPoint(free_points.get(random_point), stone);
+			p = free_points.get(random_point);
+			board.setPoint(p.x, p.y, stone);
 			
 			if (stone == Board.FRIENDLY)
 				stone = Board.ENEMY;
@@ -46,7 +60,7 @@ public class Main{
 		for (i = 0; i < board.getSize(); i++)
 			for (j = 0; j < board.getSize(); j++){
 				p = new Point(i,j);
-				if (board.getPoint(p) == Board.EMPTY){
+				if (board.getPoint(i,j) == Board.EMPTY){
 					points.add(p);
 				}
 			}
@@ -54,18 +68,47 @@ public class Main{
 	}
 	static int getDameNumber(Point p, Board board){
 		int dame_count = 0;
-		if (board.getPoint(new Point(p.x+1, p.y)) == Board.EMPTY)
+		if (board.getPoint(p.x+1, p.y) == Board.EMPTY)
 			dame_count++;
-		if (board.getPoint(new Point(p.x-1, p.y)) == Board.EMPTY)
+		if (board.getPoint(p.x-1, p.y) == Board.EMPTY)
 			dame_count++;
-		if (board.getPoint(new Point(p.x, p.y+1)) == Board.EMPTY)
+		if (board.getPoint(p.x, p.y+1) == Board.EMPTY)
 			dame_count++;
-		if (board.getPoint(new Point(p.x, p.y-1)) == Board.EMPTY)
+		if (board.getPoint(p.x, p.y-1) == Board.EMPTY)
 			dame_count++;
 
 		return dame_count;
+	}
+	static boolean isFriendlySingleEyePoint(Point p, Board board){
+		boolean is_friendly = true;
+		if (board.getPoint(p.x+1, p.y) == Board.ENEMY)
+			is_friendly = false;
+		if (board.getPoint(p.x-1, p.y) == Board.ENEMY)
+			is_friendly = false;
+		if (board.getPoint(p.x, p.y+1) == Board.ENEMY)
+			is_friendly = false;;
+		if (board.getPoint(p.x, p.y-1) == Board.ENEMY)
+			is_friendly = false;
 
+		return is_friendly;
 	}
 
+
+	static boolean couldMoveGetDame(Point p, Board board){
+		Board new_board = new Board(board);
+
+		new_board.setPoint(p.x, p.y, Board.FRIENDLY); //TODO: Always friendly?
+		removeDeadStones(new_board);
+		if (getDameNumber(p, new_board) == 0)
+			return false;
+		/*if (board.isKO(new_board))
+			return false;*/
+
+		return true;
+	}
+	static void removeDeadStones(Board board){
+		int i,j;
+
+	}
 }
 
