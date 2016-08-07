a26031f73d875b99b32fc579284a5b5562e2a396
galvanom <galvanom@mail.ru>
2016-08-07 18:46:03 +0300

    Added function for empty area spotting

--- com/letitgo/heuristics/Heuristics.java
+++ com/letitgo/heuristics/Heuristics.java
@@ -1,6 +1,8 @@
 package com.letitgo.heuristics;
 import java.io.*;
 import java.util.*;
+import com.letitgo.Point;
+import com.letitgo.Board;
 
 public class Heuristics{
 	public Pattern33 pattern33;
@@ -9,5 +11,25 @@ public class Heuristics{
 	public Heuristics(){
 		this.pattern33 = new Pattern33();
 		this.capture = new Capture();
+	}
+	// TODO: Completly not tested
+	public boolean isEmptyArea(Board board, Point p, int dist){
+		if (p.i < 0 || p.j < 0 || p.i >= board.getSize() || p.j >= board.getSize()){
+			return true;
+		}
+		if (dist >= 0){
+			return true;
+		}
+			ArrayList<Point> neighbours = p.getNeighbours();
+			for (Point neighbour: neighbours){
+				if (board.getPoint(neighbour) != Board.EMPTY){
+					return false;
+				}
+				if (isEmptyArea(board,neighbour,dist-1) == false){
+					return false;
+				}
+			}
+		return true;
+
 	}
 }
