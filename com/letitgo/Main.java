95e632743ef6a07a2603206a6783573a9c635cea
galvanom <galvanom@mail.ru>
2016-09-08 16:28:34 +0300

    commit test 2

--- com/letitgo/Main.java
+++ com/letitgo/Main.java
@@ -24,21 +24,6 @@ public class Main{
 		// }
 		//board.loadFromFile("board9x9.dat");
 
-		System.out.println("Please choose your side. Type X for black or O for white:");
-		char side = System.in.read();
-		if (side != 'X' && side != 'O'){
-			System.out.println("Wrong choice. Type X for black or O for white:");
-			return;
-		}
-		if (side == 'X'){
-			humanStone = Board.ENEMY;
-			aiStone = Board.FRIENDLY;
-			humanMove(board, humanStone);
-		}
-		if (side == 'O`'){
-			humanStone = Board.FRIENDLY;
-			aiStone = Board.ENEMY;
-		}
 
 		while (true){
 		
@@ -53,64 +38,8 @@ public class Main{
 			}
 
 			System.out.println(System.currentTimeMillis() - startTime);
-			/*
-			p = mc.getWinner();
-			
-			if (p == null){
-				System.out.println("Error. Next move is Null\n");
-			}
-			else{
-				p.printPoint();
-				board.makeMove(p, Board.ENEMY);
-				board.printBoard();
-	
-				//board.printBoard();
-				//mc.printTree();
-			}
-<<<<<<< HEAD
-			while (true){
-				System.out.println("Set next Move for O:");
-				humanMove = readNextMove(board);
-				if (!board.checkRules(humanMove, Board.FRIENDLY)){
-					System.out.println("Your move is illegal!");
 
-				}
-				else{
-					break;
-				}
-			}
-
-			board.makeMove(humanMove,Board.FRIENDLY);
-			board.printBoard();
-		*/
-=======
-
-			
-			humanMove(board, humanStone);
->>>>>>> origin/master
-
-		}		
-	
-		// Pattern33 pat = new Pattern33("patterns33.dat");
-		// pat.createPatternsFile("patterns33.dat");
-		// int[] patterns = pat.readPatternsFile("patterns33.dat");
-
-		// if (patterns != null){
-		// 	for (int pattern : patterns){
-		// 		System.out.printf("%d ",pattern);
-		// 	}
-		// }
-
-		// Pattern33.stringToNumber("XO.#");
-		// char[] pattern = {'X','O','?','X','.','.','x','.','?'};
-		// char[] pattern_ = {'.','O','.','X','.','.','.','.','.'};
-		// ArrayList<String> allVariations = new ArrayList<String>();
-		// if(!pat.getAllVariations(pattern_,0,allVariations)){
-		// 	allVariations.add(String.valueOf(pattern_));
-		// }
-		// for (String variation : allVariations){
-		// 	System.out.println(variation);
-		// }
+		}
 
 	}
 	public static Point readNextMove(Board board){
