7f6843b3bf1bb5d530b879015b580e826a5ea2dc
galvanom <galvanom@mail.ru>
2016-09-29 17:22:44 +0300

    Changes to build file

--- buildrun_win.bat
+++ buildrun_win.bat
@@ -1,4 +1,5 @@
 REM Копирование patterns33.dat нужно для DraGo, который не понимает несколько classpath т.к. в шоке от знака ";"
 REM Для работы через GTP уажите опцию -gtp в конце команды
-
-javac -d build com\letitgo\Main.java && copy /y res\patterns33.dat build && java -cp build;res com.letitgo.Main -gtp
+mkdir build
+copy /y res\patterns33.dat build
+javac -d build com\letitgo\Main.java && java -cp build;res com.letitgo.Main
